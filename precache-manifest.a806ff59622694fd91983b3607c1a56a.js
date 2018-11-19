b1fafa725f8679ef62a27766c5e08a330f1cc9b6
Manuel Savino <manuel@manuels-mbp-2.lan>
2018-11-18 22:26:45 -0500

    Updates

---
+++ precache-manifest.a806ff59622694fd91983b3607c1a56a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "c57593ecc06b08488c5a",
+    "url": "/react-gh-pages/static/js/main.c57593ec.chunk.js"
+  },
+  {
+    "revision": "107bc46de0c5eafb67cf",
+    "url": "/react-gh-pages/static/js/1.107bc46d.chunk.js"
+  },
+  {
+    "revision": "c57593ecc06b08488c5a",
+    "url": "/react-gh-pages/static/css/main.3e515ee0.chunk.css"
+  },
+  {
+    "revision": "809be9a0a97178e206f50a04385f42c5",
+    "url": "/react-gh-pages/index.html"
+  }
+];
