25e7c3afeb7648ed6f6a9441c897092cccce9904
Manuel Savino <manuel@manuels-mbp-2.lan>
2018-11-18 22:35:43 -0500

    Updates

---
+++ precache-manifest.4cc07548b164c5f9d78431a502c824eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ea1ee8d444e021097a2",
+    "url": "/ReactMovies/static/js/runtime~main.6ea1ee8d.js"
+  },
+  {
+    "revision": "0587415809f66e56d49b",
+    "url": "/ReactMovies/static/js/main.05874158.chunk.js"
+  },
+  {
+    "revision": "d8f0e8ed4db74961527c",
+    "url": "/ReactMovies/static/js/1.d8f0e8ed.chunk.js"
+  },
+  {
+    "revision": "0587415809f66e56d49b",
+    "url": "/ReactMovies/static/css/main.3e515ee0.chunk.css"
+  },
+  {
+    "revision": "debcc643814ae454b88d0e5f078347dc",
+    "url": "/ReactMovies/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a806ff59622694fd91983b3607c1a56a.js"
+  "/ReactMovies/precache-manifest.4cc07548b164c5f9d78431a502c824eb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/ReactMovies/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
